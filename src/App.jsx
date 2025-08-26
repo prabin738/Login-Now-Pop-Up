062efe594c20b9d09c83cb6d13049d289ea149e9
prabin738 <prabinsingh750@gmail.com>
2025-08-26 11:08:38 +0545

    initial app setted up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="modalOverlay">Hello</div>
+    </div>
+  );
+};
+
+export default App;
