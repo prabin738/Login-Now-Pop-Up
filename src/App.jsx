056576682bae95541fe16d746d99577cdec88fe1
prabin738 <prabinsingh750@gmail.com>
2025-08-26 11:33:21 +0545

    logic build properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const App = () => {
+  let [modalstatus, setModalstatus] = useState(false);
+
   return (
     <div className="App">
-      <div className="modalOverlay">Hello</div>
+      <button onClick={() => setModalstatus(!modalstatus)} className="en">
+        Enquire Now
+      </button>
+      <div className={`modalOverlay ${modalstatus ? "modal-show" : ""} `}></div>
+      <div className={`modal-div ${modalstatus ? "show-modal-div" : ""} `}>
+        <h3>Enquiry Now</h3>
+      </div>
     </div>
   );
 };
